ed3c84fd2d5032cba90a2210279ed4f55785c1c1
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-05-08 05:48:27 +0800

    Added arun

--- pkg/mutexdr/mutex.go
+++ pkg/mutexdr/mutex.go
@@ -13,3 +13,16 @@ func (mutex *Mutex[T]) Run(f func(old T) (new T)) {
 
 	mutex.value = f(mutex.value)
 }
+
+func (mutex *Mutex[T]) ARun(f func(old T) (new T)) chan<- T {
+	c := make(chan T)
+	go func(c chan T) {
+		mutex.mutex.Lock()
+		defer mutex.mutex.Unlock()
+
+		mutex.value = f(mutex.value)
+		c <- mutex.value
+	}(c)
+
+	return c
+}
