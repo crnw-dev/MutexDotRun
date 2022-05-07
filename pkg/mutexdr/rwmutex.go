b0d26195029c80ae0a96711c1fdd6052ad01bc18
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-05-08 05:50:49 +0800

    Added awrun and arrun

--- pkg/mutexdr/rwmutex.go
+++ pkg/mutexdr/rwmutex.go
@@ -20,3 +20,29 @@ func (mutex *RWMutex[T]) RRun(f func(old T)) {
 
 	f(mutex.value)
 }
+
+func (mutex *Mutex[T]) AWRun(f func(old T) (new T)) chan<- T {
+	c := make(chan T)
+	go func(c chan T) {
+		mutex.mutex.Lock()
+		defer mutex.mutex.Unlock()
+
+		mutex.value = f(mutex.value)
+		c <- mutex.value
+	}(c)
+
+	return c
+}
+
+func (mutex *Mutex[T]) ARRun(f func(old T) (new T)) chan<- struct{} {
+	c := make(chan struct{})
+	go func(c chan struct{}) {
+		mutex.mutex.Lock()
+		defer mutex.mutex.Unlock()
+
+		mutex.value = f(mutex.value)
+		close(c)
+	}(c)
+
+	return c
+}
