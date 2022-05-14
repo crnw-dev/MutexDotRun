9cea58ef65c0285d86852ed0c4db0646ff0d48a8
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-05-14 12:59:41 +0800

    Added TestRW

--- pkg/mutexdr/rwmutex_test.go
+++ pkg/mutexdr/rwmutex_test.go
@@ -1,7 +1,37 @@
 package mutexdr
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
+
+func testRW(
+	rw *RW[int],
+	t *testing.T,
+	expect int,
+) {
+	var wg sync.WaitGroup
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+
+		go func() {
+			v := rw.Load()
+			rw.Run(func(int) int {
+				return v + 1
+			})
+
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	v := rw.Load()
+	if v != expect {
+		t.Fatalf("Expect RW's value to be %v, got %v", expect, v)
+	}
+}
 
 func TestRW(t *testing.T) {
-
+	rw := NewRW[int]()
+	testRW(&rw, t, 3)
 }
