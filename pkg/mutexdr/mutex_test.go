d8499b4257e2c25ad73e1c55fc3259e1f9da1409
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-05-14 12:39:26 +0800

    Added c3

--- pkg/mutexdr/mutex_test.go
+++ pkg/mutexdr/mutex_test.go
@@ -8,24 +8,28 @@ func TestMutex(t *testing.T) {
 	w := NewW[int]()
 	c1 := make(chan struct{})
 	c2 := make(chan struct{})
+	c3 := make(chan struct{})
 
-	go func() {
-		w.Run(func(int) int {
-			close(c1)
-
-			return 1
-		})
-	}()
 	go func() {
 		<-c1
 		w.Run(func(int) int {
 			close(c2)
 
-			return 2
+			return 1
 		})
 	}()
+	go func() {
+		close(c1)
 
-	<-c2
+		<-c2
+		w.Run(func(int) int {
+			return 2
+		})
+
+		close(c3)
+	}()
+
+	<-c3
 	w.Run(func(v int) int {
 		const expect = 2
 		if v != expect {
